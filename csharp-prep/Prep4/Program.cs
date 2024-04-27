4f7adc9f4440341c4ce61fa7154cb8b0503275ce
anasabinomonteiro <mon23034@byui.edu>
2024-04-27 20:02:16 -0300

    complete 5 steps of assignment week 01

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write(" Please, enter a number (enter 0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            // Only add if the number is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"the sum is: {sum}.");        
+      
+        // Compute the average
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}.");
+
+        // Find max
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number> max)
+            {
+                //if the number is greather than max, max is found
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is {max}.");
+    }       
 }
