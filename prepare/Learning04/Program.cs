ed5919e21d340b5da0a5c8781e6b7337f2e9666e
anasabinomonteiro <mon23034@byui.edu>
2024-05-14 21:08:30 -0300

    Activity w04 - base class and derived classes

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
-    }
+        Assignment assignment = new Assignment("Doglas Monteiro", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());    
+    
+        MathAssignment mathAssignment = new MathAssignment("Doglas Monteiro", "Multiplication", "8.4", "6-14");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());    
+
+    
+        WritingAssignment writingAssignment = new WritingAssignment("Benjamin Monteiro", "African History", "Rescue of Ancestry");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+    }    
 }
